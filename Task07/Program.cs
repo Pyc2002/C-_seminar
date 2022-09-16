ffa61c92e44890964608b28fd6d624ae5c215f18
Chirak <Pyc2002@yandex.ru>
2022-09-16 16:03:09 +0300

    Add Task 07 - % остаток от деления

---
+++ Task07/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 100 && num < 1000) // проверка на трехзначность (от 100 до 999)
+{
+    int res = num % 10;
+    Console.WriteLine($"последняя цифра: {res}");
+}
+else
+{
+    Console.WriteLine("введено не трехзначное число");
+}
