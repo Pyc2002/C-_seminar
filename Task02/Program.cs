3e2883ba02244e8fefe0d1b0c755a6f9f5e6f02a
Chirak <Pyc2002@yandex.ru>
2022-09-16 16:23:05 +0300

    Add Task 02 - If

---
+++ Task02/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдает, 
+// какое число большее, а какое меньшее.
+// 
+// Пример:
+// a = 5; b = 7 -> max = 7
+// a = 2; b = 10 -> max = 10
+// a = -9; b = -3 -> max = -3
+
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine($"max = {a}");
+}
+else if (a < b)
+{
+    Console.WriteLine($"max = {b}");
+}
+else
+{
+    Console.WriteLine("числа равны");
+}
