330ccaf97bea5a9d332802f1079317fab3239cf5
Chirak <Pyc2002@yandex.ru>
2022-09-24 19:37:07 +0300

    Add Task 23  - Cubing

---
+++ Task23/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 0) Console.WriteLine("Ошибка! Введите положительное число");
+else
+{
+void Square (int number)
+{
+    int index = 1;
+    while (index <= number)
+    {
+        Console.WriteLine($"{index,3} -> {Math.Pow(index,3),5}");
+        index++;
+    }
+    return;
+}
+Square(num);
+}
