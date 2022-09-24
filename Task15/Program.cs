83a61aa467c96689dfc04d7e3f4c9e62bbb0e97a
Chirak <Pyc2002@yandex.ru>
2022-09-24 19:32:02 +0300

    Add Task 22

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -7,24 +7,24 @@
 
 // Первый способ без дней недели:
 
-// Console.WriteLine("Введите номер дня недели: "); 
-// int day = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите номер дня недели: "); 
+int day = Convert.ToInt32(Console.ReadLine());
 
-// if (day < 1 || day > 7) Console.WriteLine("Введен неправильный номер дня неделии. Введите число от 1 до 7"); 
-// else if (day == 6 || day == 7) Console.WriteLine("Да, выходной");
-// else Console.WriteLine("Нет, не выходной");
+if (day < 1 || day > 7) Console.WriteLine("Введен неправильный номер дня неделии. Введите число от 1 до 7"); 
+else if (day == 6 || day == 7) Console.WriteLine("Да, выходной");
+else Console.WriteLine("Нет, не выходной");
 
 // Второй способ:
 
-Console.Write("Введите номер дня недели: ");
-string day = Console.ReadLine();
+// Console.Write("Введите номер дня недели: ");
+// string day = Console.ReadLine();
 
-if (day == "1") Console.WriteLine("Нет, понедельник - не выходной");
-else if (day == "2") Console.WriteLine("Нет, вторник - не выходной");
-else if (day == "3") Console.WriteLine("Нет, среда - не выходной");
-else if (day == "4") Console.WriteLine("Нет, четверг - не выходной");
-else if (day == "5") Console.WriteLine("Нет, пятница - не выходной");
-else if (day == "6") Console.WriteLine("Да, суббота - выходной");
-else if (day == "7") Console.WriteLine("Да, воскресенье - выходной");
-else Console.WriteLine("Введен неправильный номер дня неделии. Введите число от 1 до 7");
+// if (day == "1") Console.WriteLine("Нет, понедельник - не выходной");
+// else if (day == "2") Console.WriteLine("Нет, вторник - не выходной");
+// else if (day == "3") Console.WriteLine("Нет, среда - не выходной");
+// else if (day == "4") Console.WriteLine("Нет, четверг - не выходной");
+// else if (day == "5") Console.WriteLine("Нет, пятница - не выходной");
+// else if (day == "6") Console.WriteLine("Да, суббота - выходной");
+// else if (day == "7") Console.WriteLine("Да, воскресенье - выходной");
+// else Console.WriteLine("Введен неправильный номер дня неделии. Введите число от 1 до 7");
 
