3c2bea87ad53faa29fec63d55ed557d0bd56d6b0
Chirak <Pyc2002@yandex.ru>
2022-09-24 19:33:47 +0300

    Add Task 19 - CountDown

---
+++ Task19/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 19 Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+// Первый вариант:
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int Countdown (int number)
+{   
+    int res = 0;
+    while (number > 0)
+    {
+        res = res * 10 + number % 10;
+        number /= 10;                  // number = number / 10;
+    }
+    return res;
+}
+    int result = Countdown(num);
+    if (num == result) Console.WriteLine($"Да, число {num} - палиндром");
+    else Console.WriteLine($"Нет, число {num} -не палиндром");
+
+  
+// Второй вариант: 
+
+// Console.WriteLine("Введите пятизначное число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// 
+// if(num < 10000 || num > 100000) Console.WriteLine ($"Введено не пятизначное число");
+// else
+// {
+//     void Compare(int number)
+//     {
+//         if (number / 10000 == number % 10 && number / 1000 % 10 == number / 10 % 10) Console.WriteLine($"Да, число {number} - палиндром");
+//         else Console.WriteLine($"Нет, число {number} -не палиндром");
+//         return;
+//     }
+//     Compare(num);
+// }
