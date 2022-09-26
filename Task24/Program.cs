7b95d3344833982f965e55882f6421e032f781d4
Chirak <Pyc2002@yandex.ru>
2022-09-26 20:41:05 +0300

    Add Task 24 - For

---
+++ Task24/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 24: Напишите программу, которая
+// принимает на вход число (А) и выдаёт сумму чисел
+// 
+// от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+Console.WriteLine ("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumNumbers(int num)
+{
+    int sum = default;  // значение не определено
+    for (int i = 1; i <= num; i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
+}
