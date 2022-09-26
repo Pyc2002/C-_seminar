ddba3fdf0bb172bd9661c1fa749d44bda78d7e2e
Chirak <Pyc2002@yandex.ru>
2022-09-26 22:06:11 +0300

    Add Task 30/1 - Method + Array

--- Task30/Program.cs
+++ Task30/Program.cs
@@ -4,3 +4,30 @@
 
 // [1,0,1,1,0,1,0,0]
 
+// int[]array1 = {1,2,3,4,5,6,7,8};
+// int[]array2 = new int[8] {1,2,3,4,5,6,7,8};
+// var array3 = new int[8]; // используется только, когда понятен тип данных
+
+// int[] array = new int[8];
+
+// for (int i = 0; i < array.length; i++)
+// {
+    
+// }
+
+int[] array = new int[8];
+
+FillArrayRandom();
+PrintArray();
+
+void FillArrayRandom()
+{
+    for (int i = 0; i <= array.Length - 1; i++) array[i] = new Random().Next(0, 2);
+}
+
+void PrintArray()
+{
+    Console.Write($"[");
+    for (int i = 0; i <= array.Length - 2; i++) Console.Write($"{array[i]},");
+    Console.Write($"{array[array.Length - 1]}]");
+}
