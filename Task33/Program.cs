27882c72ab00d555b0fadff3a63214a7d89fc1f7
Chirak <Pyc2002@yandex.ru>
2022-09-30 11:06:19 +0300

    Add Task 33 - Find element in array

---
+++ Task33/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая
+// определяет, присутствует ли заданное число в массиве.
+
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// 3; массив [6, 7, 19, 345, 3] -> да
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    var rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i <= array.Length - 2; i++) Console.Write($"{array[i]},");
+    Console.Write($"{array[array.Length - 1]}]");
+}
+
+Console.Write("Введите искомое число в массиве: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+bool FindDigit(int[] array, int number)
+{
+    for (int i = 0; i < array.Length; i++)
+    
+        if (number == array[i]) return true;
+        return false;
+    
+}
+
+int[] arrayGlobal = CreateArrayRndInt(10, -3, 3);
+
+PrintArray(arrayGlobal);
+if (FindDigit(arrayGlobal, num))
+    Console.WriteLine(" -> Да");
+else Console.WriteLine(" -> Нет");
