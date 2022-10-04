62e0319ff17fe2fc33db16b0377118b1e7ad38f2
Chirak <Pyc2002@yandex.ru>
2022-10-04 10:43:52 +0300

    Add Task 42 - Getting Binary number

---
+++ Task42/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+
+Console.Write("Введите целое положительное число:");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+// void ConvertToBin(int num)
+// {
+//     string bin = default;
+
+//     if (num > 0)
+//     {
+//         if (num % 2 == 1) bin += "1";
+//             else bin+= "0";
+//         ConvertToBin(num / 2);    
+//     }
+//      Console.Write(bin);
+// }
+
+void ConvertToBin2(int num)
+{
+    string bin = default;
+
+     if (num > 0)
+     {
+        bin += num % 2;         
+        ConvertToBin2(num / 2);    
+     }   
+
+    Console.Write(bin);
+}
+
+// ConvertToBin(numA);
+ConvertToBin2(numA);
