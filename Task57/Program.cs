64b34f87154139767c0cd6b520bba7cc28fbf89e
Chirak <Pyc2002@yandex.ru>
2022-10-11 09:23:21 +0300

    Add Task 57 - Find and count Elements

---
+++ Task57/Program.cs
@@ -0,0 +1,99 @@
+﻿// Задача 57: Составить частотный словарь элементов
+// двумерного массива. Частотный словарь содержит
+// информацию о том, сколько раз встречается элемент
+// входных данных.
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    var rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++) // 0 - rows  0 
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) // 1 - columns
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4} | ");
+            else Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int[] GetArrayFromMatrix(int[,] matrix)
+{
+    int[] result = new int[matrix.Length];
+
+    int index = 0;
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+
+            result[index] = matrix[i, j];
+            index++;
+
+        }
+
+    }
+
+    return result;
+
+}
+
+void CountNPrintArrayGlossary(int[] localarray)
+{
+    int num = localarray[0];
+    int counter = 1;
+    for (int i = 1; i < localarray.Length; i++)
+    {
+        if (num == localarray[i])
+        {
+            counter++;
+        }
+        else
+        {
+            Console.WriteLine($"{num} повторяется {counter} раз");
+            num = localarray[i];
+            counter = 1;
+        }
+    }
+Console.WriteLine($"{num} повторяется {counter} раз");
+
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine($"]");
+}
+
+
+
+int[,] array2D = CreateMatrixRndInt(3, 3, 0, 10);
+int[] array = GetArrayFromMatrix(array2D);
+Array.Sort(array);
+
+PrintMatrix(array2D);
+Console.WriteLine("");
+PrintArray(array);
+Console.WriteLine("");
+CountNPrintArrayGlossary(array);
