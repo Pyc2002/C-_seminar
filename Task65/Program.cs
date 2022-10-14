9102c18c35e4b1495d861bbe8217642be0715944
Chirak <Pyc2002@yandex.ru>
2022-10-14 22:46:50 +0300

    Add Task 65 - Recursion Numbers from M to N

---
+++ Task65/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 65: Задайте значения M и N. Напишите программу, которая
+// выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 6, 7, 8"
+
+
+Console.Write("Введите число M:  ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N:  ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+RangeNaturalNumbers(numberM, numberN);
+
+void RangeNaturalNumbers(int num1, int num2)
+{
+    if (num1 < num2)
+    {
+        RangeNaturalNumbers(num1, num2 - 1);
+        Console.Write($"{num2} ");
+    }
+    if (num1 > num2)
+    {
+        Console.Write($"{num1} ");
+        RangeNaturalNumbers(num1 - 1, num2);
+    }
+    if (num1 == num2)
+    {
+        Console.Write($"{num2} ");
+    }
+}
